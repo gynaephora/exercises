7c82cfc857ee054f33912d77dc428689ecd1c047
Volodymyr Korzhovskyy <it.mustango@gmail.com>
2016-08-23 00:34:45 +0300

    Display pair number 0 to 20

---
+++ src/exercises/DisplayPairNumber.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class DisplayPairNumber {
+	 public static void main(String[] args) {
+		 
+		 int i=11;
+		 int n=20;
+		 String end=" ";
+		 while(i>0){
+	        System.out.print((n-2*(i-1))+end);
+		    i--;
+		 }
+		 }
+}
