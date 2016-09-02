2460d179d1060f7ccc77a3a7fe3112b00ed4b576
Volodymyr Korzhovskyy <it.mustango@gmail.com>
2016-09-03 01:34:51 +0300

    Bruce Eckel: HelloDate

---
+++ src/exercises/HelloDate.java
@@ -0,0 +1,10 @@
+package exercises;
+import java.util.*;
+
+public class HelloDate {
+  public static void main(String[] args){
+	  System.out.println("Hello, today: ");
+	  System.out.println(new Date());
+	  
+  }
+}
